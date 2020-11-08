eccade8395a2a95a5e60a801cc596374c7bb3b40
madebyfabian <beer.fabian@mail.de>
2020-11-08 10:39:59 +0100

    added "vue-focus-lock" and "vue-inline-svg"

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import './registerServiceWorker'
 
 
+// <inline-svg>
+import InlineSvg from 'vue-inline-svg'
+Vue.component('inline-svg', InlineSvg)
+
+
 // FocusVisible
 import FocusVisible from 'vue-focus-visible'
 Vue.use(FocusVisible)
